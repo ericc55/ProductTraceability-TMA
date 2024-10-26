cadb831a376ffac1067fbc7613f53eb764ba335b
Ericc <69812528+ericc55@users.noreply.github.com>
2024-10-27 08:13:14 +0900

    Show public key

--- src/components/ConnectMetamask.vue
+++ src/components/ConnectMetamask.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <el-button v-if="!account" @click="enableEthereum" class="enableEthereumButton" type="success">MetaMaskに接続</el-button>
     <h2 v-if="account">
-      <!-- Account: <span class="showAccount">{{ account }}</span> -->
+      <el-alert type="info" title="Account public-key:" :description="account" />
       <el-alert type="success" title="ウォレットアドレスの認証：" description="この接続により、サービスがあなたのウォレットアドレスを一意のIDとして識別するための認証が完了しました。" />
-      <el-alert type="info" title="ネットワークの互換性チェック：" description="現在接続しているブロックチェーンネットワーク（例: Ethereum Mainnet）が、このサービスと互換性があると確認されました。" />
+      <el-alert type="info" title="ネットワークの互換性チェック：" description="現在接続しているブロックチェーンネットワーク（例: Ethereum Mainnet)が、このサービスと互換性があると確認されました。" />
       <el-alert type="warning" title="プライバシー保護：" description="このサービスは、あなたのウォレットアドレスのみを参照しており、プライベートキーやその他の個人情報には一切アクセスしません。" />
     </h2>
   </div>
